67ddddafccbbdd66f66e37b2dbddf25f129f3ca1
Zetra416 <penguin_vs_yeti@yahoo.com>
2020-02-28 23:44:25 +0700

    menambah file ss unit 3 javaScript introduction dan file function.js

---
+++ function.js
@@ -0,0 +1,29 @@
+
+//Tugas 1
+function shoutOut(){
+  console.log("Halo Function!");
+}
+shoutOut();
+
+// //Tugas 2
+// var num1 = 5;
+// var num2 = 6;
+//
+// function calculateMultiply(num1, num2) {
+// var res = num1*num2;
+// return res;
+// }
+// console.log(calculateMultiply(num1, num2));
+
+// //Tugas 3
+// var name ="Agus";
+// var age ="30";
+// var address = "Jln. Malioboro, Yogjakarta";
+// var hobby ="gaming";
+// var fullSentence;
+//
+// function processSentence(n, a, ad, h) {
+//   console.log(n, a, ad, h);
+// }
+//
+// processSentence(name, age, address, hobby);
